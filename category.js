80cd8af9fb3db68673f917e786f0f4472f957eaf
BluegrassMediaInc <54914562+BluegrassMediaInc@users.noreply.github.com>
2021-12-13 00:49:09 +0500

    first commit

---
+++ category.js
@@ -0,0 +1,136 @@
+import { hooks } from '@bigcommerce/stencil-utils';
+import CatalogPage from './catalog';
+import { showAlertModal } from './global/modal';
+import compareProducts from './global/compare-products';
+import FacetedSearch from './common/faceted-search';
+import { createTranslationDictionary } from '../theme/common/utils/translations-utils';
+
+export default class Category extends CatalogPage {
+    constructor(context) {
+        super(context);
+        this.validationDictionary = createTranslationDictionary(context);
+    }
+
+    setLiveRegionAttributes($element, roleType, ariaLiveStatus) {
+        $element.attr({
+            role: roleType,
+            'aria-live': ariaLiveStatus,
+        });
+    }
+    addToCart () {
+        console.log(allProdutsId);
+        if (allProdutsId.length) {
+            var cartUrl = '/cart.php'
+            for (let i = 0; i < allProdutsId.length; i++) {
+                $.ajax({
+                    type: 'GET',
+                    async: false,
+                    url: cartUrl + '?action=add&product_id=' + allProdutsId[i] + 'fastcart=1&ajaxsubmit=1',
+                    success: function (data) {
+                        //parse bigcommerce html reponse
+                        var obj = JSON.parse($(data).html());
+                        //success property = true if item was added successfully
+                        if (obj.success) {
+                            showAlertModal('All Products Added!');
+                        } else {
+                            showAlertModal('Something went wrong!');
+                            //do something to show items were not successfully added to cart
+                        }
+                    }
+                });
+            }
+        }
+        window.location.replace(cartUrl);
+    };
+
+    addAllProductsToCart() {
+        if($('#add-to-cart-button')) {
+            $('#add-to-cart-button').on('click', () => this.addToCart());
+        }
+    }
+
+    makeShopByPriceFilterAccessible() {
+        if (!$('[data-shop-by-price]').length) return;
+
+        if ($('.navList-action').hasClass('is-active')) {
+            $('a.navList-action.is-active').focus();
+        }
+
+        $('a.navList-action').on('click', () => this.setLiveRegionAttributes($('span.price-filter-message'), 'status', 'assertive'));
+    }
+
+    onReady() {
+        this.arrangeFocusOnSortBy();
+
+        $('[data-button-type="add-cart"]').on('click', (e) => this.setLiveRegionAttributes($(e.currentTarget).next(), 'status', 'polite'));
+
+        this.makeShopByPriceFilterAccessible();
+        this.addAllProductsToCart();
+        compareProducts(this.context);
+
+        if ($('#facetedSearch').length > 0) {
+            this.initFacetedSearch();
+        } else {
+            this.onSortBySubmit = this.onSortBySubmit.bind(this);
+            hooks.on('sortBy-submitted', this.onSortBySubmit);
+        }
+
+        $('a.reset-btn').on('click', () => this.setLiveRegionsAttributes($('span.reset-message'), 'status', 'polite'));
+
+        this.ariaNotifyNoProducts();
+    }
+
+    ariaNotifyNoProducts() {
+        const $noProductsMessage = $('[data-no-products-notification]');
+        if ($noProductsMessage.length) {
+            $noProductsMessage.focus();
+        }
+    }
+
+    initFacetedSearch() {
+        const {
+            price_min_evaluation: onMinPriceError,
+            price_max_evaluation: onMaxPriceError,
+            price_min_not_entered: minPriceNotEntered,
+            price_max_not_entered: maxPriceNotEntered,
+            price_invalid_value: onInvalidPrice,
+        } = this.validationDictionary;
+        const $productListingContainer = $('#product-listing-container');
+        const $facetedSearchContainer = $('#faceted-search-container');
+        const productsPerPage = this.context.categoryProductsPerPage;
+        const requestOptions = {
+            config: {
+                category: {
+                    shop_by_price: true,
+                    products: {
+                        limit: productsPerPage,
+                    },
+                },
+            },
+            template: {
+                productListing: 'category/product-listing',
+                sidebar: 'category/sidebar',
+            },
+            showMore: 'category/show-more',
+        };
+
+        this.facetedSearch = new FacetedSearch(requestOptions, (content) => {
+            $productListingContainer.html(content.productListing);
+            $facetedSearchContainer.html(content.sidebar);
+
+            $('body').triggerHandler('compareReset');
+
+            $('html, body').animate({
+                scrollTop: 0,
+            }, 100);
+        }, {
+            validationErrorMessages: {
+                onMinPriceError,
+                onMaxPriceError,
+                minPriceNotEntered,
+                maxPriceNotEntered,
+                onInvalidPrice,
+            },
+        });
+    }
+}
